1dfdafb4d40bd7cb4e09a438b3b98ddc55f55b4a
Vitor Gomes <vitorgomes_silva@outlook.com>
2024-01-09 17:30:12 -0300

    Update NovenaView View, resizing the candle to half the size of the screen. Start AboutDeveloperView View, adding a Memoji image of the developer and an about text

--- Sao Judas Tadeu Americana/NovenaView.swift
+++ Sao Judas Tadeu Americana/NovenaView.swift
@@ -47,86 +47,96 @@ struct NovenaView: View {
             }
             
             VStack {
-                // TODO: Need to draw the candle using the flame and checkboxes
-                Image(systemName: "flame.fill")
-                    .foregroundColor(.orange)
-                    .symbolEffect(.pulse) // TODO: This might distract the user. Make some test to decide remove or not. Maybe make it slower
                 
-                Spacer()
+                VStack {
+                    // Testing purposes to make candle half size of screen, so it will be easier to users reach all days with fingers
+                    // TODO: Maybe add a text or image here to populate the screen
+                    // TODO: Maybe make this a configuration, so user can set the way he want, full size or not. Probable can add the caret simbol (^) at the top, in a dropdown button
+                }.frame(maxHeight: screenSize.height * 0.5)
                 
-                // Should has 9 days
-                // TODO: Add a logic where only the present day of Novena is enabled, the past days are disabled. If you mark a day as done, the past ones get disabled. Also change the color when disabled. There is another option to make the screen follow a candle logic, where the flame starts on the top and when the person marks a day completed, it shrinks
-                Toggle(isOn: $firstDay) { // TODO: Remember to add the string on this line. I believe is for voiceOver
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15) // TODO: Change later with new style, i belive minWidth and maxWidth will be necessary
-                    // TODO: Make the toggle go down
-                    // TODO: Test with bigger checkbox sizes
-                    .foregroundColor(.black)
+                VStack {
+                    // TODO: Need to draw the candle using the flame and checkboxes
+                    Image(systemName: "flame.fill")
+                        .foregroundColor(.orange)
+                        .symbolEffect(.pulse) // TODO: This might distract the user. Make some test to decide remove or not. Maybe make it slower
+                    
+                    Spacer()
+                    
+                    // Should has 9 days
+                    // TODO: Add a logic where only the present day of Novena is enabled, the past days are disabled. If you mark a day as done, the past ones get disabled. Also change the color when disabled. There is another option to make the screen follow a candle logic, where the flame starts on the top and when the person marks a day completed, it shrinks
+                    Toggle(isOn: $firstDay) { // TODO: Remember to add the string on this line. I believe is for voiceOver
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15) // TODO: Change later with new style, i belive minWidth and maxWidth will be necessary
+                        // TODO: Make the toggle go down
+                        // TODO: Test with bigger checkbox sizes
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $secondDay) {
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15)
+                        .foregroundColor(.black)
+                        .disabled(true)
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $thirdDay) {
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15)
+                        .foregroundColor(.black)
+                        .disabled(true)
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $fourthDay) {
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15)
+                        .foregroundColor(.black)
+                        .disabled(true)
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $fifthay) {
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15)
+                        .foregroundColor(.black)
+                        .disabled(true)
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $sixthDay) {
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15)
+                        .foregroundColor(.black)
+                        .disabled(true)
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $seventhDay) {
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15)
+                        .foregroundColor(.black)
+                        .disabled(true)
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $eighthDay) {
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15)
+                        .foregroundColor(.black)
+                        .disabled(true)
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $ninthDay) {
+                    }.toggleStyle(iOSCheckboxToggleStyle())
+                        .frame(maxWidth: screenSize.width * 0.15)
+                        .foregroundColor(.black)
+                        .disabled(true)
+                        .padding(.bottom, 30) // TODO: Change 30 for a dynamic number based on User's screen size
+                }.frame(maxHeight: screenSize.height * 0.5)
                 
-                Spacer()
-                
-                Toggle(isOn: $secondDay) {
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15)
-                    .foregroundColor(.black)
-                    .disabled(true)
-                
-                Spacer()
-                
-                Toggle(isOn: $thirdDay) {
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15)
-                    .foregroundColor(.black)
-                    .disabled(true)
-                
-                Spacer()
-                
-                Toggle(isOn: $fourthDay) {
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15)
-                    .foregroundColor(.black)
-                    .disabled(true)
-                
-                Spacer()
-                
-                Toggle(isOn: $fifthay) {
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15)
-                    .foregroundColor(.black)
-                    .disabled(true)
-                
-                Spacer()
-                
-                Toggle(isOn: $sixthDay) {
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15)
-                    .foregroundColor(.black)
-                    .disabled(true)
-                
-                Spacer()
-                
-                Toggle(isOn: $seventhDay) {
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15)
-                    .foregroundColor(.black)
-                    .disabled(true)
-                
-                Spacer()
-                
-                Toggle(isOn: $eighthDay) {
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15)
-                    .foregroundColor(.black)
-                    .disabled(true)
-                
-                Spacer()
-                
-                Toggle(isOn: $ninthDay) {
-                }.toggleStyle(iOSCheckboxToggleStyle())
-                    .frame(maxWidth: screenSize.width * 0.15)
-                    .foregroundColor(.black)
-                    .disabled(true)
-                    .padding(.bottom, 30) // TODO: Change 30 for a dynamic number based on User's screen size
             }
         }
     }
