dcf9ef2c97d71eda3160bb114c64dba71f53009c
Vitor Gomes <vitorgomes_silva@outlook.com>
2024-01-24 17:49:41 -0300

    Fix a bug on AboutView where tapping on any area of the View causes it to return to MainView by removing the entire content inside a VStack located inside the List. Add a camera button inside the Map of AboutView that allows the camera to return to the Church’s point

--- Sao Judas Tadeu Americana/Views/HomeViews/AboutView.swift
+++ Sao Judas Tadeu Americana/Views/HomeViews/AboutView.swift
@@ -10,56 +10,68 @@ import MapKit
 
 struct AboutView: View {
     
+    let churchLocation = CLLocationCoordinate2D(latitude: -22.750035511019487, longitude: -47.358945254521316)
     @Environment (\.dismiss) var dismiss
+    @State var mapCamera: MapCameraPosition = .automatic
     
     var body: some View {
         // TODO: Fix view spacing between the elements
         List {
-            VStack {
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies laoreet pretium. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean eget lectus nibh. Donec feugiat hendrerit lorem. Donec luctus libero a rutrum molestie. Quisque dictum euismod eros, sit amet luctus neque dignissim a. Fusce feugiat ut felis vitae congue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse malesuada scelerisque mollis. Maecenas quis efficitur nisi. Phasellus quis viverra neque. Integer nec justo arcu. Proin scelerisque eu turpis sed sollicitudin. In scelerisque elit id metus hendrerit sollicitudin. Vivamus non lobortis libero. Donec ut scelerisque sapien. Aenean dignissim hendrerit diam at accumsan. Nullam euismod interdum est, quis sodales odio convallis id. Etiam nec placerat nisi.")
-                    .listRowSeparator(.hidden) // TODO: Replace Lorem Ipsum text
-                    // TODO: Add font style
-                    // TODO: Test put images from the church between the texts paragraph, like a paragraph and an image and so on
-                HStack { // TODO: Make these as buttons, not only images
-                    Image("instagramLogo")
-                        .resizable()
-                        .scaledToFill()
-                        //.clipShape(Circle())
-                        .frame(width: 70, height: 70)
-                    Image("whatsappLogo")
-                        // TODO: Create a function to replace all copied code
-                        .resizable()
-                        .scaledToFill()
-                        .clipShape(Circle())
-                        .frame(width: 70, height: 70)
-                    Image("facebookLogo")
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies laoreet pretium. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean eget lectus nibh. Donec feugiat hendrerit lorem. Donec luctus libero a rutrum molestie. Quisque dictum euismod eros, sit amet luctus neque dignissim a. Fusce feugiat ut felis vitae congue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse malesuada scelerisque mollis. Maecenas quis efficitur nisi. Phasellus quis viverra neque. Integer nec justo arcu. Proin scelerisque eu turpis sed sollicitudin. In scelerisque elit id metus hendrerit sollicitudin. Vivamus non lobortis libero. Donec ut scelerisque sapien. Aenean dignissim hendrerit diam at accumsan. Nullam euismod interdum est, quis sodales odio convallis id. Etiam nec placerat nisi.")
+                .listRowSeparator(.hidden) // TODO: Replace Lorem Ipsum text
+                // TODO: Add font style
+                // TODO: Test put images from the church between the texts paragraph, like a paragraph and an image and so on
+            HStack { // TODO: Make these as buttons, not only images
+                Spacer()
+                Image("instagramLogo")
+                    .resizable()
+                    .scaledToFill()
+                    //.clipShape(Circle())
+                    .frame(width: 70, height: 70)
+                Image("whatsappLogo")
                     // TODO: Create a function to replace all copied code
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(Circle())
+                    .frame(width: 70, height: 70)
+                Image("facebookLogo")
+                // TODO: Create a function to replace all copied code
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(Circle())
+                    .frame(width: 70, height: 70)
+                Spacer()
+            }.listRowSeparator(.hidden)
+            //.background(Color.brown) // TODO: Decide a background color for the entire app
+        
+            Map() {
+                Marker("Paróquia São Judas Tadeu", coordinate: churchLocation)
+            }
+            .frame(width: 300, height: 200, alignment: .center) // TODO: Edit fixed size
+            .safeAreaInset(edge: .bottom, alignment: .trailing) {
+                Button {
+                    mapCamera = .region(MKCoordinateRegion(center: churchLocation, latitudinalMeters: 100, longitudinalMeters: 100)) // TODO: Not working, need to investigate
+                } label: {
+                    Image(systemName: "location.circle.fill")
                         .resizable()
-                        .scaledToFill()
-                        .clipShape(Circle())
-                        .frame(width: 70, height: 70)
-                }.listRowSeparator(.hidden)
-                //.background(Color.brown) // TODO: Decide a background color for the entire app
+                        .foregroundColor(.black) // TODO: Edit for Dark Mode
+                        .frame(maxWidth: 30, maxHeight: 30) // TODO: Add dynamic size
+                }.background(Color.brown) // TODO: Only for tests. Remove later
+                    .padding(20)
+                    
+            }
             
-                Map() {
-                    Marker("Paróquia São Judas Tadeu", coordinate: CLLocationCoordinate2D(latitude: -22.750035511019487, longitude: -47.358945254521316))
-                }
-                .frame(width: 300, height: 200, alignment: .center) // TODO: Edit fixed size
-                // TODO: Add a custom pin for the map
+            HStack {
+                Spacer()
                 
-                HStack {
-                    Spacer()
-                    
-                    Button {
-                        dismiss()
-                    } label: {
-                        Text("Voltar")
-                    }
-                    .foregroundColor(.black) // TODO: Need to implement color for dark mode
-                    
-                    Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Text("Voltar")
                 }
-                //.listRowSeparator(.visible) // TODO: Not showing after MapView, need to investigate
+                .foregroundColor(.black) // TODO: Need to implement color for dark mode
+                
+                Spacer()
             }
         }// .scrollContentBackground(.hidden) // TODO: Test it later
             .navigationTitle("Sobre")
